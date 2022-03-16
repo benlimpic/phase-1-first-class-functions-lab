91793ad9b4d71548b3d96f5b0946e430a5a3f016
init.defaultBranch <benlimpic@gmail.com>
2022-03-16 11:02:47 -0700

    commit tree 49c05009bf2ae5ba33d9615fffcb96aee525daf7

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(drivers.length - 2, drivers.length);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (integer) => {
+    return function (fare) {
+        return integer * fare
+    };
+}
+
+// let x = createFareMultiplier(6);
+// console.log(x(100));
+
+const fareDoubler = (fare) => {
+    return createFareMultiplier(2)(fare) ;
+}
+
+const fareTripler = (fare) => {
+    return createFareMultiplier(3)(fare);
+}
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers);
+}
